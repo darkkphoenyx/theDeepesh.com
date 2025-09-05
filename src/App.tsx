eafe0aa25d421d0b5e810b9969ab393c04557fd1
Deepesh Sunuwar <deepgeneral33@gmail.com>
2025-09-05 15:37:58 +0545

    Preloader added

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Cursor from "./components/Cursor";
 import Homepage from "./pages/Homepage";
 import Aos from "aos";
@@ -6,17 +6,35 @@ import "aos/dist/aos.css";
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
 import Lenis from "lenis";
+import PreLoader from "./pages/sections/PreLoader/PreLoader";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+
   useEffect(() => {
-    Aos.init({
-      duration: 1000,
-      once: false,
-    });
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 2400);
+
+    return () => clearTimeout(timer);
   }, []);
 
   useEffect(() => {
+    if (!loading) {
+      Aos.init({
+        duration: 1000,
+        once: false,
+      });
+
+      setTimeout(() => {
+        Aos.refresh();
+      }, 100);
+    }
+  }, [loading]);
+
+  useEffect(() => {
     const lenis = new Lenis();
+
     function raf(time: any) {
       lenis.raf(time);
       requestAnimationFrame(raf);
@@ -24,12 +42,12 @@ function App() {
 
     requestAnimationFrame(raf);
   }, []);
+
   return (
     <>
+      {loading && <PreLoader />}
       <Cursor />
-      <Provider store={store}>
-        <Homepage />
-      </Provider>
+      <Provider store={store}>{!loading && <Homepage />}</Provider>
     </>
   );
 }
