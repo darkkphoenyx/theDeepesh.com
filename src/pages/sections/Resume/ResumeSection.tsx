a8f785ea0e3ead3c1db04e9826bc9812f7a13656
Deepesh Sunuwar <deepgeneral33@gmail.com>
2025-11-20 21:48:44 +0545

    skills icons marquee changed for mobile

--- src/pages/sections/Resume/ResumeSection.tsx
+++ src/pages/sections/Resume/ResumeSection.tsx
@@ -106,8 +106,9 @@ const ResumeSection = () => {
       {/* skills marquee */}
       <Marquee direction="left" className="md:py-6">
         {SkillsImages.map((image) => (
-          <div className="my-10 mx-20">
-            <image.logo size={64} />
+          <div key={image.text} className="my-10 md:mx-20 mx-10">
+            <image.logo size={64} className="hidden md:block" />
+            <image.logo size={40} className="block md:hidden" />
           </div>
         ))}
       </Marquee>
